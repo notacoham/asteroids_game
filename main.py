51115da6608237a787421995ca50ebb2761f9e04
notacoham <alexcottam12@gmail.com>
2025-06-14 11:26:24 -0500

    add rotation and player position updates

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_x = SCREEN_WIDTH / 2
+    player_y = SCREEN_HEIGHT / 2
+    player = Player(player_x, player_y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+        player.update(dt)
 if __name__ == "__main__":
     main()
